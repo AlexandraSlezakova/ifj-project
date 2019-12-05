22e99fe467d17cf4a4de5c65d7de3c6b92110b24
Martin Vaďura <martin.vadura14gmail.com>
2019-12-05 21:35:59 +0100

    Fix Return param

---
+++ posfix_func.c
@@ -0,0 +1,4 @@
+//
+// Created by god on 05.12.19.
+//
+
---
+++ posfix_func.h
@@ -0,0 +1,8 @@
+//
+// Created by god on 05.12.19.
+//
+
+#ifndef SCANNER_C_POSFIX_FUNC_H
+#define SCANNER_C_POSFIX_FUNC_H
+
+#endif //SCANNER_C_POSFIX_FUNC_H
