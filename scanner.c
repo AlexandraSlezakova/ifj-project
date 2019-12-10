af03bf1df4b29c8260cfca8c07f216ebfd1790d4
Alexandra <alexandra.slezak@gmail.com>
2019-12-10 15:31:27 +0100

    escape sekvencia v stringu

--- scanner.c
+++ scanner.c
@@ -401,6 +401,28 @@ int get_token()
                 /* escape sequence */
             case S_ESC:
                 if (c == '"' || c == 'n' || c == 't' || c == '\\' || c == 39) {
+                    buffer = realloc(buffer, (size_t) ++allocated);
+                    buffer = realloc(buffer, (size_t) ++allocated);
+
+                    iterator--;
+                    buffer[iterator] = '0';
+                    if (c == '"') {
+                        buffer[++iterator] = '3';
+                        buffer[++iterator] = '4';
+                    } else if (c == 'n') {
+                        buffer[++iterator] = '1';
+                        buffer[++iterator] = '0';
+                    } else if (c == 't') {
+                        buffer[++iterator] = '0';
+                        buffer[++iterator] = '9';
+                    } else if (c == '\\') {
+                        buffer[++iterator] = '9';
+                        buffer[++iterator] = '2';
+                    } else if (c == 39) {
+                        buffer[++iterator] = '3';
+                        buffer[++iterator] = '9';
+                    }
+                    iterator++;
                     state = S_STRING_CONTENT;
                 }
                 else if (c == 39) {
